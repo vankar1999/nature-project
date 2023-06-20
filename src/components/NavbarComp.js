d4ff4b81898c2f0ca9e3a2b8a874ac2e91bef242
Sathambakkam <KARUNAKARAN.SATHAMBAKKAM@LibertyMutual.com>
2023-06-20 10:25:00 -0700

    navigation functionality added

--- src/components/NavbarComp.js
+++ src/components/NavbarComp.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { useState } from 'react';
 import { Navbar, Nav,NavDropdown,Form,FormControl,Button} from 'react-bootstrap';
 //import{Switch,BrowserRouter as Router,Route,Link}  from "react-router-dom"
 
@@ -6,11 +6,41 @@ import Home from './Home';
 import Mountains from './Mountains';
 import Parks from './Parks';
 
- function NavbarComp(){
+function NavbarComp(){
 
+    const [currentPageName, setCurrentPageName] = useState("home")
+    const handleHome = () => {setCurrentPageName("home")}
+    const handleMountains = () => {setCurrentPageName("mountains")}
+    const handleParks = () => {setCurrentPageName("parks")}
+  
+    let currentPage = null;
+    switch(currentPageName){
+      case "home": 
+        currentPage = <Home/>;
+        break;
+      case "parks": 
+        currentPage = <Parks/>;
+        break;
+      case "mountains": 
+        currentPage = <Mountains/>;
+        break;
+      
+    }
     return (
-        <div>"Hello World"</div>
-    );
+  
+        <div>
+        <nav>
+          <ul>
+           <li><a href = "#" onClick={handleHome}> Home </a></li> 
+           <li><a href = "#" onClick={handleMountains}> Mountains </a></li> 
+           <li><a href = "#" onClick={handleParks}> Parks </a></li> 
+          </ul>
+        </nav>
+        {currentPage}
+
+        </div>
+
+    )
 
 };
 
